e189293b97af51049654fc1b1bc41d54f8bbf5e4
Hans Kristian Flaatten <hans@starefossen.com>
2013-07-07 15:05:44 +0200

    Adds /opt/bin to $PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,4 +10,5 @@ set -x LANGUAGE en_US.UTF-8
 
 set -x PATH $PATH ~/.rvm/bin # Add RVM to PATH for scripting
 set -x PATH $PATH /usr/local/share/CodeSourcery/bin #Adds gcc path
+set -x PATH $PATH /opt/bin
 set -x NODE_PATH $NODE_PATH /usr/lib/node_modules
