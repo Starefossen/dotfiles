e749a9ab13c1b4273a84b9add4d66c0bed347966
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-05-31 16:02:34 +0200

    Add Docker Machine development environment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,7 @@ set -x LANGUAGE en_US.UTF-8
 set -x PATH $PATH ~/bin/ # Local bin directory
 set -x PATH $PATH /opt/bin/ # Tmux path
 
+set -x DOCKER_TLS_VERIFY "1";
+set -x DOCKER_HOST "tcp://192.168.99.100:2376";
+set -x DOCKER_CERT_PATH "/Users/hans/.docker/machine/machines/dev";
+set -x DOCKER_MACHINE_NAME "dev";
