d9c695df1ec8aad197dc7ca4e8d21badd4f2b194
Hans Kristian Flaatten <hans.flaatten@evry.com>
2017-12-27 07:58:41 +0100

    fish: remove custom PATH overrides

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,9 +13,6 @@ set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x LANGUAGE en_US.UTF-8
 
-set -x PATH $PATH ~/bin/ # Local bin directory
-set -x PATH $PATH /opt/bin/ # Tmux path
-
 set -x DOCKER_TLS_VERIFY "1";
 set -x DOCKER_HOST "tcp://172.16.10.129:2376";
 set -x DOCKER_CERT_PATH "/Users/hans/.docker/machine/machines/default";
