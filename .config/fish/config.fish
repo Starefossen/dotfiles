e1f40d3131825dc65355474bf1d3c6a3aaa58e2c
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-01-04 14:34:23 +0100

    fish: set FZF default command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,3 +17,5 @@ set -x DOCKER_TLS_VERIFY "1";
 set -x DOCKER_HOST "tcp://192.168.99.100:2376";
 set -x DOCKER_CERT_PATH "/Users/hans/.docker/machine/machines/default";
 set -x DOCKER_MACHINE_NAME "default";
+
+set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
