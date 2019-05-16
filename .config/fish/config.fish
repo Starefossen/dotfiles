1eea60e43deaaf72e70ede8fb22db654398aeb55
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-05-16 22:37:26 +0200

    fish: move Docker specific config to Docker config file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,9 +13,4 @@ set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x LANGUAGE en_US.UTF-8
 
-set -x DOCKER_TLS_VERIFY "1";
-set -x DOCKER_HOST "tcp://192.168.99.100:2376";
-set -x DOCKER_CERT_PATH "/Users/hans/.docker/machine/machines/default";
-set -x DOCKER_MACHINE_NAME "default";
-
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
