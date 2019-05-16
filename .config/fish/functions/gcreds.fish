620fd6d1338ce000195fa03fe8aa3c84159c47d1
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-05-16 22:38:00 +0200

    gcreds: add support for loading encryption key

--- .config/fish/functions/gcreds.fish
+++ .config/fish/functions/gcreds.fish
@@ -1,12 +1,22 @@
 #!/usr/bin/fish
 
 function gcreds -d "Set google credentials"
-  gcloud config set project $argv[1] > /dev/null
+  set project $argv[1]
 
-  set path ~/.gcloud/$argv[1].json
-  if test -f $path
-    set -gx GOOGLE_APPLICATION_CREDENTIALS $path
-    set -gx GOOGLE_CREDENTIALS (cat $path | tr -d '\n')
+  gcloud config set project $project > /dev/null
+  set -gx TERRAFORM_STATE_GCP_BUCKET $project-tf-state
+
+  set encryption_key ~/.gcloud/$project.enc
+  if test -f $encryption_key
+    set -gx GOOGLE_ENCRYPTION_KEY (cat $encryption_key)
+  else
+    set -ge GOOGLE_ENCRYPTION_KEY
+  end
+
+  set credentials ~/.gcloud/$project.json
+  if test -f $credentials
+    set -gx GOOGLE_APPLICATION_CREDENTIALS $credentials
+    set -gx GOOGLE_CREDENTIALS (cat $credentials | tr -d '\n')
   else
     echo "File $path does not exist...."
   end
