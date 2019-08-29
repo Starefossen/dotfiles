e3e1e553970f58b23d7c4e7e1fd8c238fd673074
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-08-29 18:12:25 +0200

    fish: add terraform v0.12 function

---
+++ .config/fish/functions/terraform12.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+
+function terraform12 -d "Run terraform command"
+  set mnt /root/project
+  set cmd terraform
+
+  set env_file
+  if test -f .env
+    set env_file "--env-file=.env"
+  end
+
+  docker run -it --rm \
+    -v (pwd):$mnt \
+    -v ~/.helm:/root/.helm \
+    -v ~/.kube:/root/.kube \
+    -v ~/.terraform.d:/root/.terraform.d \
+    -w /$mnt \
+    -e GOOGLE_CREDENTIALS \
+    -e GOOGLE_ENCRYPTION_KEY \
+    (env | grep TF_ | cut -f1 -d= | sed 's/^/-e /') \
+    $env_file \
+    hashicorp/terraform:latest $argv
+end
