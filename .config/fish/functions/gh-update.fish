d91a5af9a2c5462714623c529a22fea290c3a65b
Hans Kristian Flaatten <hans.flaatten@evry.com>
2021-01-01 13:21:48 +0100

    gh: update the updater

--- .config/fish/functions/gh-update.fish
+++ .config/fish/functions/gh-update.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
 
 function gh-update -d "Update gh to latest version"
-  set ghVersion (curl -v https://api.github.com/repos/cli/cli/releases/latest | jq -r '.name' | awk -F "v" '{ print $2 }')
+  set ghVersion (curl -v https://api.github.com/repos/cli/cli/releases/latest | jq -r '.tag_name' | cut -c2-)
+  echo "Updating to version $ghVersion..."
   curl -vOL "https://github.com/cli/cli/releases/download/v"$ghVersion"/gh_"$ghVersion"_macOS_amd64.tar.gz"
   tar vxf "gh_"$ghVersion"_macOS_amd64.tar.gz"
   sudo mv "gh_"$ghVersion"_macOS_amd64/bin/gh" /usr/local/bin/
