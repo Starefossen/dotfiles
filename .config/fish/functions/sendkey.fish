21027ee90484a347e0b2f9939015d598f72322f0
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2022-10-21 14:48:26 +0200

    fish: update sendkey function

--- .config/fish/functions/sendkey.fish
+++ .config/fish/functions/sendkey.fish
@@ -1,16 +1,18 @@
 #!/usr/bin/env fish
 
 function sendkey -d "Send ssh public key to some remote host (ssh-copy-id alternative)"
-  if test -f ~/.ssh/id_rsa.pub
+  set fileName ~/.ssh/id_ed25519.pub
+
+  if test -f $fileName
     if test (count $argv) -lt 2
       set port 22
     else
       set port $argv[2]
     end
     if test (count $argv) -gt 0
-      ssh $argv[1] -p $port "mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; cat >> ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
+      ssh $argv[1] -p $port "mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; cat >> ~/.ssh/authorized_keys" < $fileName
     end
   else
-    echo "There is no ~/.ssh/id_rsa.pub, please generate your keys with 'ssh-keygen'"
+    echo "There is no " $filename ", please generate your keys with 'ssh-keygen'"
   end
 end
