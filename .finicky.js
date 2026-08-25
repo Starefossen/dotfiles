e2b6fa063c7688d650ebdb4805f7ef81b486d9fe
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2026-08-25 11:10:50 +0200

    feat(finicky): configure domain routing for Safari Profiles (nav, dev, social, personal)

--- .finicky.js
+++ .finicky.js
@@ -1,21 +1,63 @@
 module.exports = {
-  // Use Safari as the default browser for everything
-  defaultBrowser: "Safari",
+  // Your "Personal" profile is the fallback for everything else
+  defaultBrowser: {
+    name: "Safari",
+    profile: "Personal"
+  },
   
+  options: {
+    // Hide the finicky icon from the menu bar
+    hideIcon: false,
+    checkForUpdate: true,
+  },
+
   handlers: [
     {
-      // Open work-related links in Firefox Developer Edition
+      // NAV (Work) Profile
       match: [
         "*.nav.no*",
         "*github.com/navikt*",
         "*.slack.com*",
         "*slack-edge.com*",
-        "*zoom.us*"
+        "*zoom.us*",
+        "*.nais.io*"
       ],
-      browser: "Firefox Developer Edition"
+      browser: {
+        name: "Safari",
+        profile: "Nav"
+      }
     },
     {
-      // Open Figma links directly in the Figma desktop app
+      // Developer Profile
+      match: [
+        "github.com*",
+        "*.github.com*",
+        "stackoverflow.com*",
+        "*.aws.amazon.com*",
+        "console.cloud.google.com*"
+      ],
+      browser: {
+        name: "Safari",
+        profile: "Developer"
+      }
+    },
+    {
+      // Social Profile
+      match: [
+        "*.facebook.com*",
+        "*.twitter.com*",
+        "*.x.com*",
+        "bsky.app*",
+        "*.linkedin.com*",
+        "*.instagram.com*"
+      ],
+      browser: {
+        name: "Safari",
+        profile: "Social"
+      }
+    },
+    {
+      // Figma Links -> Native App
       match: "https://www.figma.com/file/*",
       browser: "Figma",
     }
